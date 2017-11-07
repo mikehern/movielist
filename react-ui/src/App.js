bf3644684a4505f9144466cf3cd817fbfbf15b2a
Mike Hernandez <mkhern@gmail.com>
2017-11-07 00:25:37 -0800

    Completed level 0

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const movies = [
+  { title: 'Mean Girls' },
+  { title: 'Hackers' },
+  { title: 'The Grey' },
+  { title: 'Sunshine' },
+  { title: 'Ex Machina' }
+];
+
+function MovieList(props) {
+  const movies = props.movies;
+  const listItems = movies.map((movie) => 
+    <li>{movie.title}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Movielist</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit Guisak John Pricehoun and save to reload.
         </p>
+        <MovieList movies={movies} />
       </div>
     );
   }
